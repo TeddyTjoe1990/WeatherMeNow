08e35d92139002098b261703c4362e2b5ee5811c
Jannuar Teddy Herjanto <97140849+TeddyTjoe1990@users.noreply.github.com>
2023-12-17 02:07:02 +0700

    Update about.tsx

--- weatherapp/pages/about.tsx
+++ weatherapp/pages/about.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+// pages/about.tsx
+import React from 'react';
+import Weather from '../components/Layout';
+import Layout from '../components/Layout';
 
 const AboutPage = () => (
-  <Layout defaultCity="About | Next.js + TypeScript Example">
+  <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
     <p>This is the about page</p>
-    <p>
-      <Link href="/">Go home</Link>
-    </p>
+    <Weather title="Weather Information" defaultCity="Default City" />
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
